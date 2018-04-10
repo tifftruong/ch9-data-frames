f5e56159684d6b9cd659479dbfc61831380ae640
Tiffany Truong <melissatruong@D-69-91-131-73.dhcp4.washington.edu>
2018-04-09 17:15:11 -0700

    Use Bill Gates grant data set

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -4,36 +4,42 @@
 # file into a variable called `grants` using the `read.csv()`
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
-
+grants <- read.csv('data/gates_money.csv', stringsAsFactors = FALSE)
 
 # Use the View function to look at the loaded data
-
+View(grants)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
-
+organization <- grants$organization
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
 # This is a useful debugging tip if you hit errors later!
-
+is.vector(organization)
 
 ## Now you can ask some interesting questions about the dataset
 
 # What was the mean grant value?
-
+mean <- mean(grants$total_amount)
+#2600196.6
 
 # What was the dollar amount of the largest grant?
-
+largest_amount <- max(grants$total_amount)
+#100000000
 
 # What was the dollar amount of the smallest grant?
-
+smallest_amount <- min(grants$total_amount)
+#5000
 
 # Which organization received the largest grant?
-
+largest_organization <- organization[grants$total_amount == largest_amount]
+#Hillsborough County Public Schools
 
 # Which organization received the smallest grant?
-
+smallest_organization <- organization[grants$total_amount == smallest_amount]
+#New Mexico Business Roundtable for Educational ...
 
 # How many grants were awarded in 2010?
-
+length(grants$total_amount[grants$start_year == 2010])
+#18
